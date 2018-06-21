6a20271989f660ada89d6e10bdcdd552fff80a44
Fangxin <fx920915>
2018-06-21 17:06:56 +0800

    2018年6月21日17:06:31  获取验证码加时间计时 60s

--- views/common/login.js
+++ views/common/login.js
@@ -2,11 +2,37 @@
     'use strict';
 
     app.controller('loginCtrl', ['$rootScope', '$scope', '$state', 'REST', function ($rootScope, $scope, $state, REST) {
-            REST.logout();
+        var clock = '';
+        var second = 60;
+        var btn;
 
-        $('.loginName').focus();
-        $scope.getCode = function () {
-            alert();
+        function doLoop() {
+            second--;
+            if (second > 0) {
+                btn.innerHTML = second + '秒后可重新获取';
+            } else {
+                clearInterval(clock);
+                btn.disabled = false;
+                btn.innerHTML = '获取验证码';
+                second = 60;
+            }
+        }
+
+        $scope.getCode = function (ev) {
+            // $('body').loading({
+            //     title:'请稍等',
+            //     name:'test1',
+            //     discription:'正在获取..',
+            // });
+
+            setTimeout(function(){
+                removeLoading('test1');
+            },1000);
+            btn = ev.target;
+            console.log(btn.innerHTML);
+            btn.disabled = true;
+            btn.innerHTML = second + '秒后可重新获取';
+            clock = setInterval(doLoop, 1000);
         };
 
         $scope.login = function (name, pwd) {
