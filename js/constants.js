f6ae63e24482ae05615ea1012f1ca986067ed441
Fangxin <fx920915>
2018-06-27 19:12:36 +0800

    2018年6月27日15:13:14  方青 支持https

--- js/constants.js
+++ js/constants.js
@@ -3,6 +3,7 @@
   app
     .constant('RS', {
         // ip: 'http://192.168.1.14:8084/'  //EK
+        // ip: 'http://192.168.1.52:8084/'  //EK
         ip: 'https://www.duodingfen.com/fintech-app/'  // 服务器
     })
 })();
